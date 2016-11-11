3620023a5486f61767b1a9340e16b60ef55ed67e
tiagodealmeida <tiagocdealmeida78@gmail.com>
2016-11-11 19:46:21 +0100

    4.5 Carousel and partial

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,12 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.limit(3)
   end
 
   # GET /products/1
   # GET /products/1.json
-  def show  
+  def show
   end
 
   # GET /products/new
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,6 @@ class StaticPagesController < ApplicationController
 
   def landing_page
     @featured_product = Product.first
+    @products = Product.limit(3)
   end
 end
