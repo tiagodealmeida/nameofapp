de0058fda59fa1aa3b0612f19b1858e3d070a409
tiagodealmeida <tiagocdealmeida78@gmail.com>
2016-11-15 01:06:03 +0100

    5.2 3rd commit - bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Product.create(:name => "New Bike", :description => "this is a new bike", :frame_color => "grey", :price => "99")
-Product.create(:name => "Another New Bike", :description => "this is another new bike", :frame_color => "yellow", :price => "199")
-Product.create(:name => "Yet Another New Bike", :description => "this is yet another new bike", :frame_color => "orange", :price => "299")
-Product.create(:name => "The Last New Bike", :description => "this the last new bike", :frame_color => "pink", :price => "399")
