32dbc38eaf73e63b6361108f8de285d445c306ec
tiagodealmeida <tiagocdealmeida78@gmail.com>
2016-11-24 13:57:17 +0100

    5.8 Raty stars

--- app/assets/javascripts/site.js
+++
@@ -1,9 +0,0 @@
-$(document).on('turbolinks:load', function(){
-  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-  $('.rated').raty({ path: '/assets',
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    }
-  });
-});
