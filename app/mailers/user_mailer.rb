d414d514f54a347bf9a7285964824e85ca053fbb
tiagodealmeida <tiagocdealmeida78@gmail.com>
2016-11-25 20:30:31 +0100

    5.11 Consumer Web App

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,14 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
   @message = message
     mail(:from => email,
-      :to => 'tiagocdealmeida78@gmail.com',
-      :subject => "A new contact form message from #{name}")
+         :to => 'tiagocdealmeida78@gmail.com',
+         :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+    @appname = "Bike Berlin"
+    mail( :to => user.email,
+          :subject => "Welcome to #{@appname}!")
+  end
+
 end
