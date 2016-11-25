e4103dce8e2d92f98e673ee1e1f2be0096260b33
tiagodealmeida <tiagocdealmeida78@gmail.com>
2016-11-25 15:33:00 +0100

    5.10 debug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125111129) do
+ActiveRecord::Schema.define(version: 20161125141415) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
