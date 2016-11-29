638cdc41e497c066232c7eeb2add7a3db87164bd
tiagodealmeida <tiagocdealmeida78@gmail.com>
2016-11-29 16:40:07 +0100

    6.3 Functional Test

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+
+  let(:user) { User.create!(email: 'test2@email.com', password: 'password2') }
+
+  describe 'GET #show' do
+    context 'User is logged in' do
+      before do
+        sign_in user
+      end
+
+      it 'loads correct user details' do
+        get :show, id: user.id
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq user
+      end
+
+    end
+
+    context 'No user is logged in' do
+      it 'redirects to login' do
+        get :show, id: user.id
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+  end
+
+end
