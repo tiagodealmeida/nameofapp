d27c078b1c130d104bbc522dd725fa48390ec276
tiagodealmeida <tiagocdealmeida78@gmail.com>
2016-12-01 22:14:19 +0100

    6.6 Creating Payments

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
 
   post 'static_pages/thank_you'
 
+  post 'payments/create'
+
   root 'static_pages#landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
