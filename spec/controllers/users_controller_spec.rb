8941d0d4334e9f4e8f15920a74c71721eefdd23b
tiagodealmeida <tiagocdealmeida78@gmail.com>
2016-12-01 13:50:16 +0100

    6.4 Generating Test Data - Complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,7 +4,6 @@ describe UsersController, :type => :controller do
 
   #let(:user) { User.create!(email: 'test2@email.com', password: 'password2') }
   let (:user) {FactoryGirl.create(:user)}
-  let (:other_user) { FactoryGirl.create(:user)}
 
   describe 'GET #show' do
     context 'User is logged in' do
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,10 +1,19 @@
 FactoryGirl.define do
+  sequence(:email) { |n| "user#{n}@example.com" }
   factory :user do
     email
     password "1234567890"
-    first_name "Peter"
-    last_name "Example"
+    first_name "John"
+    last_name "Doe"
     admin false
+  end
+
+  factory :admin, class: User do
+    email
+    password "qwertyuiop"
+    admin true
+    first_name "Admin"
+    last_name "User"
   end
 
 end
