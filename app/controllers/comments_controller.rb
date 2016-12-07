047d1241fbc9f8b32b91a2b2842a3a01156e78f4
tiagodealmeida <tiagocdealmeida78@gmail.com>
2016-12-07 17:12:25 +0100

    6.10 AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
+        format.js
       else
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -22,8 +23,7 @@ class CommentsController < ApplicationController
     redirect_to product
   end
 
-private
-
+  private
   def comment_params
     params.require(:comment).permit(:user_id, :body, :rating)
   end
